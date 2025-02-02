00c4da9197aab8127177f3846f6b39e04baf8bc9
Dominika Matczynska <dominika.matczynska@stxnext.pl>
2025-02-02 14:31:48 +0100

    Get cookie with waitUntil

--- cypress/e2e/checkout.cy.ts
+++ cypress/e2e/checkout.cy.ts
@@ -1,23 +1,16 @@
 import { cartPage } from "../page-objects/cart-page";
-import { homePage } from "../page-objects/home-page";
 
-describe("Checkout", () => {
+describe("Checkout test", () => {
   let form_key: string;
 
-  before(() => {
-    homePage.navigateTo();
-    // eslint-disable-next-line cypress/no-unnecessary-waiting
-    cy.wait(1000);
-    cy.getCookie("form_key").then((cookie) => {
-      if (cookie) {
-        form_key = cookie.value;
-      }
+  before("Preset cart content by API", () => {
+    cy.visit("/");
+    cy.waitUntil(() =>
+      cy.getCookie("form_key").then((cookie) => cookie?.value),
+    ).then((cookieValue: string) => {
+      form_key = cookieValue;
     });
-  });
 
-  it("Can proceed to checkout", () => {
-    const endpoint =
-      "https://magento.softwaretestingboard.com/checkout/cart/add/uenc/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS93b21lbi90b3BzLXdvbWVuL2phY2tldHMtd29tZW4uaHRtbA%2C%2C/product/1380/";
     cy.fixture("add-to-cart-payload.json").then((data) => {
       const formData = new FormData();
 
@@ -27,11 +20,15 @@ describe("Checkout", () => {
       formData.append("super_attribute[143]", data["super_attribute[143]"]);
       formData.append("super_attribute[93]", data["super_attribute[93]"]);
 
+      const endpoint = `/checkout/cart/add/uenc/${data.uenc}/product/${data.product}/`;
+
       cy.request("POST", endpoint, formData).then((response) => {
         expect(response.status).to.eq(200);
       });
     });
+  });
 
+  it("Can proceed to checkout", () => {
     const productName = "Juno Jacket";
     const size = "S";
     const color = "Blue";
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import "./commands";
 import "cypress-real-events";
+import "cypress-wait-until";
 
 Cypress.on("uncaught:exception", (err, runnable) => false);
